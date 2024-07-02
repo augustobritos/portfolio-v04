3e4b5b76d2f74e1694fb6dbc7d6b7b7126f3c19d
augustobritos <augustobritos95@gmail.com>
2024-07-02 20:49:17 -0300

    add metadata and siteConfig

---
+++ src/config/site.ts
@@ -0,0 +1,24 @@
+export const siteConfig = {
+  name: "Augusto's Life Insights.",
+  url: "https://augustobritos.com",
+  ogImage: "https://augustobritos.com/og.jpg",
+  description: "Explore my digital realm!",
+  links: {
+    twitter: "https://twitter.com/augustobritos",
+    github: "https://github.com/augustobritos",
+  },
+  keyWords: [
+    "Web Development",
+    "Software Development",
+    "Web Artist",
+    "Developer Psychology",
+    "programming tips",
+    "developer mindset",
+  ],
+  authors: {
+    name: "Augusto Britos",
+    url: "https://augustobritos.com",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;

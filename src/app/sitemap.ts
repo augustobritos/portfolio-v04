4d6f710e6d86e97aff819c95fd0789cf7966949b
augustobritos <agusbr95@gmail.com>
2024-07-04 13:53:52 -0300

    add robots & sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,21 @@
+import { siteConfig } from "@/config/site";
+import { getAllPosts } from "./api/sanity/posts";
+
+export default async function sitemap() {
+  const baseUrl = siteConfig.url;
+
+  const posts = await getAllPosts();
+
+  const postUrls = posts?.map((post) => ({
+    url: `${baseUrl}/blog/${post?.slug}`,
+    lastModified: new Date(),
+  }));
+
+  return [
+    { url: baseUrl, lastModified: new Date() },
+    { url: `${baseUrl}/blog`, lastModified: new Date() },
+    ...postUrls,
+  ];
+}
+
+export const dynamic = "force-dynamic";

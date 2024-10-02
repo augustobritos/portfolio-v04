a893c2e68889077463e478bab93584771783c58c
augustobritos <agusbr95@gmail.com>
2024-10-02 14:22:29 -0300

    Header update.

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,19 +1,11 @@
-import {
-  PageHeader,
-  PageHeaderDescription,
-  PageHeaderHeading,
-} from "../shared/page-header";
-import StatusBadge from "../shared/status-badge";
+import { PageHeader, PageHeaderHeading } from "../shared/page-header";
 
 const Hero = () => {
   return (
     <PageHeader className="py-8">
       <PageHeaderHeading className="text-balance">
-        Mixing creativity and technology to create a meaningful impact in the world.
+        Together, We Create
       </PageHeaderHeading>
-      <PageHeaderDescription className="text-end">
-        <StatusBadge />
-      </PageHeaderDescription>
     </PageHeader>
   );
 };
